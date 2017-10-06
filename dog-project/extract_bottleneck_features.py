341a67d054500fb42f1819470ffcbaa0e244e6c0
ozkansafak <shafaksan@gmail.com>
2017-10-06 12:38:44 -0700

    submit

--- dog-project/extract_bottleneck_features.py
+++ dog-project/extract_bottleneck_features.py
@@ -1,5 +1,5 @@
 def extract_VGG16(tensor):
-	from keras.applications.vgg16 import VGG16, preprocess_input
+	from keras.applications.vgg16 import VGG16, preprocess_input 
 	return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
 def extract_VGG19(tensor):
